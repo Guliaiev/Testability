6247cb247f1cda5a04b4a6bd2fe69cd0e6ce917d
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-03-10 17:14:16 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main   {
+    public static void main(String[] args) {
+        float cost = 10000f;
+        float miles = 20f;
+        float bonus = cost / miles;
+        System.out.println("bonus miles " +bonus);
+    }}
